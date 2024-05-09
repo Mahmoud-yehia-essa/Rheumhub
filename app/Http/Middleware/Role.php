cd135c84a9b2748e2c9ef31534e59a321e094349
Mahmoud Essa <mahmoudessa@gmail.com>
2024-05-09 23:04:44 +0300

    update form

--- app/Http/Middleware/Role.php
+++ app/Http/Middleware/Role.php
@@ -17,7 +17,7 @@ class Role
     {
         if($request->user()->role !== $role)
         {
-            return redirect('dashboard');
+            return redirect('view-application');
 
         }
         return $next($request);
